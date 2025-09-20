0e7211ce1680f05dd7f67cd487cdba57462eeb9e
Karo4a <t1mati666@yandex.ru>
2025-09-20 13:11:42 +0300

    finished task1 in arrays start task2 in arrays

--- src/main/kotlin/arrays/task1.kt
+++ src/main/kotlin/arrays/task1.kt
@@ -1,20 +1,40 @@
 package arrays
 
+import kotlin.math.abs
+
 fun task1() {
+    println("Программа выводит кол-во использованных различных цифр в введенной матрице трехзначных чисел.")
     println("Ручной ввод чисел в матрицу!")
 
     print("Количество строк в матрице: ")
     val sizeRows = readln().toUIntOrNull()
     if (sizeRows != null) {
-        print("Количество столбец в матрице: ")
+        print("Количество столбцов в матрице: ")
         val sizeCols = readln().toUIntOrNull()
         if (sizeCols != null) {
             println("Вводите только трехзначные числа!")
-            val matrix = arrayOfNulls<Array<Int>>(sizeRows.toInt())
-            for (i in 0..sizeRows.toInt()) {
-
+            val matrix = arrayOfNulls<Array<Int?>>(sizeRows.toInt())
+            for (i in 0 until sizeRows.toInt()) {
+                val row = arrayOfNulls<Int>(sizeCols.toInt())
+                for (j in 0 until sizeCols.toInt()) {
+                    var inputEl : Int?
+                    do {
+                        print("arr[$i][$j] = ")
+                        inputEl = readln().toIntOrNull()
+                        val isValid = inputEl != null && abs(inputEl) in 100..999
+                        if (!isValid) {
+                            println("Введите корректное трехзначное число!")
+                        }
+                    } while (!isValid)
+                    row[j] = inputEl
+                }
+                matrix[i] = row
             }
 
+            val typedMatrix = matrix.map{ row -> row.orEmpty()}.orEmpty().toTypedArray()
+            typedMatrix.forEach { row -> println(row.joinToString(" ")) }
+            val countOfUniqueDigits = typedMatrix.flatten().joinToString("").toSet().size
+            println("В матрице используется $countOfUniqueDigits различных цифр")
         } else {
             println("Некорректный ввод количества столбцов!")
         }
--- src/main/kotlin/arrays/task2.kt
+++ src/main/kotlin/arrays/task2.kt
@@ -1,5 +1,42 @@
 package arrays
 
+import kotlin.math.abs
+
 fun task2() {
-    
+    println("Программа проверяет симметрична ли матрица относительно главной диагонали.")
+    println("Ручной ввод чисел в матрицу!")
+
+    print("Размер квадратной матрицы: ")
+    val sizeMatrix = readln().toUIntOrNull()
+    if (sizeMatrix != null) {
+        println("Вводите только однозначные числа!")
+        val matrix = arrayOfNulls<Array<Int?>>(sizeMatrix.toInt())
+        for (i in 0 until sizeMatrix.toInt()) {
+            val row = arrayOfNulls<Int>(sizeMatrix.toInt())
+            for (j in 0 until sizeMatrix.toInt()) {
+                var inputEl : Int?
+                do {
+                    print("arr[$i][$j] = ")
+                    inputEl = readln().toIntOrNull()
+                    val isValid = inputEl != null && abs(inputEl) in 1..9
+                    if (!isValid) {
+                        println("Введите корректное однозначное число!")
+                    }
+                } while (!isValid)
+                row[j] = inputEl
+            }
+            matrix[i] = row
+        }
+
+//        for (n in 0..sizeMatrix*(sizeMatrix+1)/2)) {
+//
+//        }
+
+        val typedMatrix = matrix.map{ row -> row.orEmpty()}.orEmpty().toTypedArray()
+        typedMatrix.forEach { row -> println(row.joinToString(" ")) }
+        val countOfUniqueDigits = typedMatrix.flatten().joinToString("").toSet().size
+        println("В матрице используется $countOfUniqueDigits различных цифр")
+    } else {
+        println("Некорректный ввод размера квадратной матрицы!")
+    }
 }
