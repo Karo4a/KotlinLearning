40ecb162d1dd6e956bb4e31f8039ecff387eda72
Karo4a <t1mati666@yandex.ru>
2025-10-25 15:46:38 +0300

    start task5 in arrays

--- src/main/kotlin/arrays/task5.kt
+++ src/main/kotlin/arrays/task5.kt
@@ -1,5 +1,21 @@
 package arrays
 
 fun task5() {
-    
+    println("Программа группирует слова введенные пользователем по признаку 'состоят из одинаковых букв'")
+
+    print("Введите количество вводимых слов: ")
+    val quantityU = readln().toUIntOrNull()
+    if (quantityU != null) {
+        val quantity = quantityU.toInt()
+        val words = mutableListOf<String>()
+
+        for (i in 0..quantity-1) {
+            print("Слово ${i+1} = ")
+            words.add(readln())
+        }
+        println(words)
+
+    } else {
+        println("Некорректный ввод!")
+    }
 }
