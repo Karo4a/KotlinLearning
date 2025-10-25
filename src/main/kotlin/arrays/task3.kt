6b1dc6e56d435a4b1e6a7d654842aa0cd1b7b0be
Karo4a <t1mati666@yandex.ru>
2025-10-25 15:37:13 +0300

    finish task3 in arrays

--- src/main/kotlin/arrays/task3.kt
+++ src/main/kotlin/arrays/task3.kt
@@ -1,15 +1,111 @@
 package arrays
 
+import kotlin.math.ceil
+
+val russianAlphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ".map{ it }.toTypedArray()
+val codeAlphabet = arrayOf(21, 13, 4, 20, 22, 1, 25, 12, 24, 14, 2, 28, 9, 23, 3, 29, 6, 16, 15, 11, 26, 5, 30, 27, 8, 18, 10, 33, 31, 32, 19, 7, 17)
+
+var toCode : Map<Char, Int> = russianAlphabet.zip(codeAlphabet).toMap()
+var toRus = codeAlphabet.zip(russianAlphabet).toMap()
+
 fun task3() {
-    val russianAlphabet = 'А'..'Я'
-    val codeAlphabet = 1..33
-
-    val codeShuffled = codeAlphabet.shuffled()
-    var toCode = russianAlphabet.zip(codeShuffled).toMap()
-    var toRus = codeShuffled.zip(russianAlphabet).toMap()
-
     println("Программа шифрует или дешифрует введенное слово по заданному ключевому слову.")
 
-    var keyWord = "ПОЛЕ"
-    var inputWord = ""
+    println("Зашифровать или расшифровать текст?")
+    println("1 - Зашифровать")
+    println("2 - Расшифровать")
+    print("Выбор: ")
+
+    val choice = readln().toIntOrNull()
+
+    if (choice  !in 1..2) {
+        println("Некорректный выбор!")
+        return
+    }
+
+    print("Введите ключевое слово: ")
+    val keyWord = readln()
+
+    if (choice == 1) {
+        print("Введите слово для зашифрования: ")
+    } else if (choice == 2) {
+        print("Введите слово для расшифрования: ")
+    }
+    val inputWord = readln().uppercase()
+
+    if (choice == 1) {
+        encode(inputWord, keyWord)
+    } else if (choice == 2) {
+        decode(inputWord, keyWord)
+    }
+}
+
+fun encode(word : String, keyWord : String) {
+    val repeats = ceil(word.length.toFloat() / keyWord.length.toFloat()).toInt()
+    val preparedKeyWord = keyWord.repeat(repeats).slice(0..word.lastIndex).uppercase()
+
+    val processedSymbols = mutableListOf<Char>()
+    var isProcessed = true
+    for ((wordChar, keyChar) in word.zip(preparedKeyWord)) {
+        val keyCode = toCode[keyChar]
+        if (keyCode != null) {
+            val inputWordCode = toCode[wordChar]
+            if (inputWordCode != null) {
+                val encodedChar = toRus[(keyCode + inputWordCode - 1) % 33 + 1]
+                if (encodedChar != null) {
+                    processedSymbols.add(encodedChar)
+                }
+            } else {
+                println("Введенное слово содержит недопустимые символы (только русский алфавит)!")
+                isProcessed = false
+                break
+            }
+        } else {
+            println("Ключевое слово содержит недопустимые символы (только русский алфавит)!")
+            isProcessed = false
+            break
+        }
+    }
+
+    if (isProcessed) {
+        val encodedWord = processedSymbols.joinToString("")
+        println("Зашифрованное слово: $encodedWord")
+    }
+}
+
+fun decode(word : String, keyWord : String) {
+    val repeats = ceil(word.length.toFloat() / keyWord.length.toFloat()).toInt()
+    val preparedKeyWord = keyWord.repeat(repeats).slice(0..word.lastIndex).uppercase()
+
+    val processedSymbols = mutableListOf<Char>()
+    var isProcessed = true
+    for ((wordChar, keyChar) in word.zip(preparedKeyWord)) {
+        val keyCode = toCode[keyChar]
+        if (keyCode != null) {
+            val inputWordCode = toCode[wordChar]
+            if (inputWordCode != null) {
+                var decodedCode = inputWordCode - keyCode
+                if (decodedCode < 1) {
+                    decodedCode += 33
+                }
+                val decodedChar = toRus[decodedCode]
+                if (decodedChar != null) {
+                    processedSymbols.add(decodedChar)
+                }
+            } else {
+                println("Введенное слово содержит недопустимые символы (только русский алфавит)!")
+                isProcessed = false
+                break
+            }
+        } else {
+            println("Ключевое слово содержит недопустимые символы (только русский алфавит)!")
+            isProcessed = false
+            break
+        }
+    }
+
+    if (isProcessed) {
+        val encodedWord = processedSymbols.joinToString("")
+        println("Дешифрованное слово: $encodedWord")
+    }
 }
