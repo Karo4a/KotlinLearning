793060a3ea2b7295641f3101bb5d558803356fc0
Karo4a <t1mati666@yandex.ru>
2025-10-26 23:39:16 +0300

    finish task5 in arrays

--- src/main/kotlin/arrays/task5.kt
+++ src/main/kotlin/arrays/task5.kt
@@ -8,12 +8,32 @@ fun task5() {
     if (quantityU != null) {
         val quantity = quantityU.toInt()
         val words = mutableListOf<String>()
+        val wordGroups = mutableMapOf<String, MutableList<String>>()
+        for (i in 0 until quantity) {
+            var word : String
+            do {
+                print("Слово ${i+1} = ")
+                word = readln()
 
-        for (i in 0..quantity-1) {
-            print("Слово ${i+1} = ")
-            words.add(readln())
+                val isWord = word.isNotEmpty() && !word.contains(' ')
+                if (!isWord) {
+                    println("Введите не пустое слово и без пробелов!")
+                }
+            } while (!isWord)
+            words.add(word)
+
+            val wordKey = word.toSortedSet().joinToString("")
+            val wordGroup = wordGroups[wordKey]
+            if (wordGroup != null) {
+                wordGroup.add(word)
+            } else {
+                wordGroups[wordKey] = mutableListOf(word)
+            }
         }
-        println(words)
+
+        for ((group, words) in wordGroups.entries) {
+            println("Группа $group: ${words.joinToString()}")
+        }
 
     } else {
         println("Некорректный ввод!")
